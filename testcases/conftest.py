a73d666b1275cf17cd335e9c963380314fb42360
byh <bao981014@163.com>
2021-02-28 20:22:45 +0800

    xiugai

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -2,12 +2,17 @@ import time
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
 
 
 @pytest.fixture()
 def init_driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
-    time.sleep(3)
+    # time.sleep(3)
     driver.quit()
 
